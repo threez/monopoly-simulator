97a1c3c455071caa57502ac48f1ddacbb0428703
Vincent Landgraf <setcool@gmx.de>
2009-01-11 23:04:12 +0100

    enhanced testcase

--- test/game.rb
+++ test/game.rb
@@ -2,12 +2,33 @@ require "../lib/playing_field"
 require "../lib/human_player"
 require "pp"
 include Monopoly
-vincent = HumanPlayer.new("Vincent")
-lisa = HumanPlayer.new("Lisa")
+
+if false
+  vincent = HumanPlayer.new("Vincent")
+  lisa = HumanPlayer.new("Lisa")
+else
+  vincent = Player.new("Vincent")
+  lisa = Player.new("Lisa")
+end
+
 players = [vincent, lisa]
 pf = PlayingField.new(players)
-1000.times { [vincent, lisa].each { |player| pf.play_turn player } }
-puts "#" * 50
-p vincent.money
-puts "#" * 50
-p lisa.money
+
+# play 1000 turns
+1000.times { players.each { |player| pf.play_turn player } }
+
+def print_player(player)
+  puts "## #{player.name} " + "#" * 50
+  puts "Values: #{player.value}"
+  puts " Streets:"
+  player.streets.each do |street|
+    puts "  - #{street.name}: value => #{street.value} "
+  end
+  puts " Can build on:"
+  player.find_buildable_streets.each do |street|
+    puts "  - #{street.name}:[#{street.color}] houses => #{street.houses} value => #{street.value} "
+  end
+end
+
+print_player vincent
+print_player lisa
